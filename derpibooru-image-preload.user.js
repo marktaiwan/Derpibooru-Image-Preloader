5f53417f7017081714650eb0d0b65b7a325e41cd
Mark Taiwan <mark.taiwan@gmail.com>
2019-02-15 23:28:44 +0800

    Version bump to 1.2.0

--- derpibooru-image-preload.user.js
+++ derpibooru-image-preload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Marker's Derpibooru Image Preloader
 // @description  Preload previous/next images.
-// @version      1.1.0
+// @version      1.2.0
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
