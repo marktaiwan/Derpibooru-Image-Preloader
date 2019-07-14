197a2c6476b760925edd409e14022bea29bf3b0b
Mark Taiwan <mark.taiwan@gmail.com>
2019-07-14 13:36:45 +0800

    Update library

--- derpibooru-image-preload.user.js
+++ derpibooru-image-preload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Marker's Derpibooru Image Preloader
 // @description  Preload previous/next images.
-// @version      1.2.3
+// @version      1.2.4
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
@@ -14,7 +14,7 @@
 // @grant        GM_addStyle
 // @inject-into  content
 // @noframes
-// @require      https://openuserjs.org/src/libs/mark.taiwangmail.com/Derpibooru_Unified_Userscript_UI_Utility.js
+// @require      https://openuserjs.org/src/libs/mark.taiwangmail.com/Derpibooru_Unified_Userscript_UI_Utility.js?v1.0.4
 // ==/UserScript==
 
 (function () {
