1b3c842b77d07dd635092d1fae7589ce5fa36b87
Mark Taiwan <marktaiwan@users.noreply.github.com>
2023-04-17 20:18:53 +0800

    Version 1.2.14

--- derpibooru-image-preload.user.js
+++ derpibooru-image-preload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Marker's Derpibooru Image Preloader
 // @description  Preload previous/next images.
-// @version      1.2.13
+// @version      1.2.14
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
